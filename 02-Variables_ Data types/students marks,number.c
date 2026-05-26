dc5fce6123a2665399b8a47b3bd4ae92ed4022d2
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 15:42:08 +0530

    Create students marks,number.c

---
+++ 02-Variables_ Data types/students marks,number.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(){
+    int roll number = 86;
+    float marks = 96.67;
+    char name[50] = "Gayathri";
+    printf("Roll Number: %d\n", roll number);
+    printf("Marks: %.2f\n", marks);
+    printf("Name: %s\n", name);
+    return 0;
+}
