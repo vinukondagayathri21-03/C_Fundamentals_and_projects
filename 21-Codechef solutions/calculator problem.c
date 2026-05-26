1bc3422eb78e30e094ddc5eb75e9684df1d73589
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 15:42:49 +0530

    Create calculator problem.c

---
+++ 21-Codechef solutions/calculator problem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a = 85, b = 23;
+    int sum;
+    int diff;
+    int product;
+    int quotient;
+    sum = a + b;
+    diff = a - b;
+    product = a * b;
+    quotient = a / b;
+    printf("%d\n", sum);
+    printf("%d\n", diff);
+    printf("%d\n", product);
+    printf("%d\n", quotient);
+    return 0;
+}
