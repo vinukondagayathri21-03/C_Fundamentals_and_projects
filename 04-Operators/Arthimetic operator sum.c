0f892a403a7f8b9326fb465975d16e8419f5bd49
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 15:42:28 +0530

    Create Arthimetic operator sum.c

---
+++ 04-Operators/Arthimetic operator sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Enter the first number:");
+    scanf("%d", &a);
+    printf("Enter the second number:");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum of %d and %d is %d", a, b, sum);
+    return 0;
+    
+}
