e31dd4171f41c3b0a2b9f50d93bdbb67d9ccf793
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 15:43:46 +0530

    Create relational operator.c

---
+++ 21-Codechef solutions/relational operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int height a = 10;
+    int height b = 99;
+    if (height a >= height b){
+        printf("height a is greater than or equal to height b");
+    }
+    else{
+        printf("height a is less than height b");
+    }
+}
