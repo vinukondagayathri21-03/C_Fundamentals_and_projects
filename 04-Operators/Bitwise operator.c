daf628febe22b5e69c07c4344930261c3b838b7f
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 16:47:05 +0530

    Create Bitwise operator.c

---
+++ 04-Operators/Bitwise operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a = 8;
+    int b = 5;
+    int result = a & b;
+    printf("The result of a & b is: %d\n", result);
+        result = a|b;
+    printf("The result of a|b is: %d\n", result);
+    return 0;
+}
