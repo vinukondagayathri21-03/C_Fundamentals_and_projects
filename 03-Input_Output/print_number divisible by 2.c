a4603e73f9dd5f39e1a583dc2012525fba6c45b0
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 15:42:24 +0530

    Create print_number divisible by 2.c

---
+++ 03-Input_Output/print_number divisible by 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Enter a number:");
+    scanf("%d", &num);
+    printf("Number divisible by 2: %d\n", num);
+        if (num % 2 == 0) {
+            printf("%d is divisible by 2", num);
+        }else{
+            printf("%d is not divisible by 2", num);
+        }
+    }
+}
