9c71225655f14841bbc167368edc9d19c40a9060
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 16:42:17 +0530

    Create increment,decrement operator.c

---
+++ 04-Operators/increment,decrement operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+    printf("The value of a is: %d\n", a);
+    a++;
+    printf("The value of a after increment is: %d\n", a);
+    a--;
+    printf("The value of a after decrement is: %d\n", a);
+    return 0;
+}
