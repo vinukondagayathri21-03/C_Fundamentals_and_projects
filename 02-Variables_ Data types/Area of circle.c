e37dd6f8f68b47aa3568cb875473c23d7d6cc00a
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-26 15:41:51 +0530

    Create Area of circle.c

---
+++ 02-Variables_ Data types/Area of circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float radius, area;
+    printf("Enter the radius of the circle:");
+    scanf("%f", &radius);
+    area = 3.14 * radius * radius;
+    printf("Area of the circle is %.2f", area);
+    return 0;
+
+}
