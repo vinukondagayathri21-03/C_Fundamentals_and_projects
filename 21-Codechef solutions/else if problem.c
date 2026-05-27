54d905f08ea818064fe904132b9ae8a06a3cf7a4
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-27 15:57:52 +0530

    Create else if problem.c

---
+++ 21-Codechef solutions/else if problem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    int b = 34;
+    if (a > b) {
+        printf("%d is greater than %d\n", a, b);
+    } else {
+        printf("%d is not greater than %d\n", a, b);
+    }
+    return 0;
+}
