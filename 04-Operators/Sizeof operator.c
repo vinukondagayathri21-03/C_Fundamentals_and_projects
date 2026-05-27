d297a681390b97e83b3ef5fb8c594f95d523c112
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-27 10:49:25 +0530

    Create Sizeof operator.c

---
+++ 04-Operators/Sizeof operator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    printf("The size of the variable a is: %lu bytes\n", sizeof(a));
+    printf("The size of the int data type is: %lu bytes\n", sizeof(int));
+    return 0;
+}
