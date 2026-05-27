2f92933e835536ad496a626885ad747c094afd18
vinukondagayathri21-03 <vinukondagayathri21@gmail.com>
2026-05-27 11:01:32 +0530

    Create check even or odd.c

---
+++ 05-Conditional-statements/check even or odd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 82;
+    if (a % 2 == 0) {
+        printf("%d is an even number.\n", a);
+    }else{
+        printf("%d is an odd number.\n", a);
+    }
+    return 0;
+}
